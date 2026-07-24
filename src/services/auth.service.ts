3bb3501e44f794ae7c9397b003dae02e4fdc9f0a
vrichie <dev.ritchiek@gmail.com>
2026-07-24 14:07:31 +0300

    clean up

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,18 +1,17 @@
 import { prisma } from "../../lib/prisma";
 import { CreateUser } from "../types";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 
-
-export const register = async (registerUser:CreateUser)=>{
-    const hashedPassword = await bcrypt.hash(registerUser.password,10);
-    const user = await prisma.user.create({
-        data:{
-            firstName:registerUser.firstName,
-            lastName:registerUser.lastName,
-            email:registerUser.email,
-            phone:registerUser.phone,
-            password:hashedPassword
-        }
-    })
-    return user;
-}
+export const register = async (registerUser: CreateUser) => {
+  const hashedPassword = await bcrypt.hash(registerUser.password, 10);
+  const user = await prisma.user.create({
+    data: {
+      firstName: registerUser.firstName,
+      lastName: registerUser.lastName,
+      email: registerUser.email,
+      phone: registerUser.phone,
+      password: hashedPassword,
+    },
+  });
+  return user;
+};
